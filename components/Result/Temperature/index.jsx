0ad405e90e34b0e0988fa246e053d2b7287b35df
Novandi Ramadhan <novandiramadhan80@gmail.com>
2024-08-16 20:07:57 +0700

    update temperature status

--- components/Result/Temperature/index.jsx
+++ components/Result/Temperature/index.jsx
@@ -1,4 +1,9 @@
 export default function Temperature({ temp }) {
+  const habitability =
+    0 <= temp && temp <= 100
+      ? "Layak huni oleh manusia, berada dalam kisaran suhu air cair pada tekanan atmosfer standar Bumi antara 0 °C dan 100 °C."
+      : "Tidak layak huni oleh manusia, tidak berada dalam kisaran suhu air cair pada tekanan atmosfer standar Bumi antara 0 °C dan 100 °C.";
+
   return (
     <div className="flex flex-col gap-2">
       <div
@@ -9,10 +14,7 @@ export default function Temperature({ temp }) {
         <span className="text-6xl">{temp}</span>
         <span className="text-2xl">°C</span>
       </div>
-      <span className="text-sm">
-        Layak huni oleh manusia, berada dalam kisaran suhu air cair pada tekanan
-        atmosfer standar Bumi antara 0 °C dan 100 °C.
-      </span>
+      <span className="text-sm">{habitability}</span>
     </div>
   );
 }
